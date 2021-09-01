dd2c4b5a10afa03153c8640659bf9ef15a25e0de
Garry <ingold333@mail.ru>
2021-09-01 11:29:09 +0800

    3 глава

---
+++ String8_1.py
@@ -0,0 +1,13 @@
+# Задача 6. Определить, сколько раз в заданной строке встречается некоторая подстрока.
+
+st = input('Введите строку: ')
+sim = input('Введите подстроку: ')
+length1=len(st)
+length2=len(sim)
+k=0
+
+for i in range(length1-length2+1):
+    if st(i:i+length2)==sim:
+        k=k+1
+
+print(k)
