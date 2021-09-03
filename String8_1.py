079318c2c6e67abc19b0bc843669b301a5fc0570
Garry <ingold333@mail.ru>
2021-09-03 10:04:43 +0800

    3 глава

--- String8_1.py
+++ String8_1.py
@@ -2,12 +2,14 @@
 
 st = input('Введите строку: ')
 sim = input('Введите подстроку: ')
-length1=len(st)
-length2=len(sim)
-k=0
+length1 = len(st)
+length2 = len(sim)
+k = 0
 
-for i in range(length1-length2+1):
-    if st(i:i+length2)==sim:
-        k=k+1
+for i in range(length1 - length2 + 1):
+    if st[i:i + length2] == sim:
+        k = k + 1
 
 print(k)
+
+
